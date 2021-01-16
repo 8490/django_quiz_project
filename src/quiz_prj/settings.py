6d4c7fc275f94b44ff54edb81a4aa2a81a15869d
Mert <mert.san3407@gmail.com>
2021-01-16 20:53:00 +0300

    added corsheader

--- src/quiz_prj/settings.py
+++ src/quiz_prj/settings.py
@@ -42,13 +42,17 @@ INSTALLED_APPS = [
     'nested_admin',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'corsheaders',
     'quiz.apps.QuizConfig',
     'users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +139,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS=True
